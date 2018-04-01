9374c09151792ec9d628f1df9a199233577e4511
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-03-31 20:26:54 -0400

    Updated app title, added test button to HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -83,7 +83,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Qwestr'
     }
   },
   name: 'App'
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,10 +11,37 @@
             </small>
           </footer>
         </blockquote>
+        <v-btn
+          fab
+          dark
+          fixed
+          bottom
+          right
+          color="green"
+          @click="buttonClick"
+        >
+          <v-icon>add</v-icon>
+        </v-btn>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    buttonClick () {
+      this.count += 1
+      console.log('Button has been pressed: ', this.count)
+    }
+  }
+}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
