25a59d3971f4a28126c517a302fd4a3b20cf4a00
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-04-02 17:32:19 -0400

    Created QwestList component and associated spec

--- src/components/Qwest.vue
+++ src/components/Qwest.vue
@@ -1,15 +1,18 @@
 <template>
   <v-container>
     <app-qwest-dialog></app-qwest-dialog>
+    <app-qwest-list></app-qwest-list>
   </v-container>
 </template>
 
 <script>
 import QwestDialog from '@/components/QwestDialog'
+import QwestList from '@/components/QwestList'
 
 export default {
   components: {
-    'app-qwest-dialog': QwestDialog
+    'app-qwest-dialog': QwestDialog,
+    'app-qwest-list': QwestList
   }
 }
 </script>
