da5bd8883ad886401e832eca7fe2189e5abf59a7
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-04-04 22:04:56 -0400

    Created/ implemented classListForHeader method

--- src/components/QwestList.vue
+++ src/components/QwestList.vue
@@ -10,41 +10,22 @@
   >
     <template slot="headers" slot-scope="props">
       <tr>
-        <th>
-          <v-checkbox
-            primary
-            hide-details
-            @click.native="toggleAll"
-            :input-value="props.all"
-            :indeterminate="props.indeterminate"
-          ></v-checkbox>
-        </th>
         <th
           v-for="header in props.headers"
           :key="header.text"
-          :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+          :class="classListForHeader(header)"
           @click="changeSort(header.value)"
         >
-          <v-icon small>arrow_upward</v-icon>
+          <v-icon v-if="header.sortable" small>
+            arrow_upward
+          </v-icon>
           {{ header.text }}
         </th>
       </tr>
     </template>
     <template slot="items" slot-scope="props">
       <tr :active="props.selected" @click="props.selected = !props.selected">
-        <td>
-          <v-checkbox
-            primary
-            hide-details
-            :input-value="props.selected"
-          ></v-checkbox>
-        </td>
         <td>{{ props.item.name }}</td>
-        <td class="text-xs-right">{{ props.item.calories }}</td>
-        <td class="text-xs-right">{{ props.item.fat }}</td>
-        <td class="text-xs-right">{{ props.item.carbs }}</td>
-        <td class="text-xs-right">{{ props.item.protein }}</td>
-        <td class="text-xs-right">{{ props.item.iron }}</td>
       </tr>
     </template>
   </v-data-table>
@@ -55,119 +36,38 @@ export default {
   data () {
     return {
       pagination: {
-        sortBy: 'name'
+        sortBy: 'name',
+        descending: true
       },
       selected: [],
       headers: [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'left',
-          value: 'name'
-        },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' }
+        { text: 'Qwest', value: 'name', align: 'left', sortable: true },
+        { text: 'Status', value: 'status', align: 'left', sortable: true },
+        { text: 'Actions', value: 'action', sortable: false }
       ],
       items: [
         {
-          value: false,
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%'
+          name: 'Find an Apartment',
+          status: 'Incomplete'
         },
         {
-          value: false,
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%'
-        },
-        {
-          value: false,
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%'
-        },
-        {
-          value: false,
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%'
-        },
-        {
-          value: false,
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%'
-        },
-        {
-          value: false,
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%'
-        },
-        {
-          value: false,
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%'
-        },
-        {
-          value: false,
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%'
-        },
-        {
-          value: false,
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%'
-        },
-        {
-          value: false,
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%'
+          name: 'Open a Persian Restaurant',
+          status: 'Complete'
         }
       ]
     }
   },
   methods: {
-    toggleAll () {
-      if (this.selected.length) this.selected = []
-      else this.selected = this.items.slice()
+    classListForHeader (header) {
+      const classList = ['column']
+      if (header.sortable) {
+        classList.push('sortable')
+      }
+      if (header.value === this.pagination.sortBy) {
+        classList.push('active')
+      }
+      this.pagination.descending ? classList.push('desc') : classList.push('asc')
+      return classList
     },
     changeSort (column) {
       if (this.pagination.sortBy === column) {
