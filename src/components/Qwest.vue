ca4f26ef09f64c4174cb77b76bc1fc9b30a2db48
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-04-16 20:06:57 -0400

    Refactored QwestDialog component to QwestCreateDialog

--- src/components/Qwest.vue
+++ src/components/Qwest.vue
@@ -1,17 +1,17 @@
 <template>
   <v-container>
-    <app-qwest-dialog></app-qwest-dialog>
+    <app-qwest-create-dialog></app-qwest-create-dialog>
     <app-qwest-list></app-qwest-list>
   </v-container>
 </template>
 
 <script>
-import QwestDialog from '@/components/QwestDialog'
+import QwestCreateDialog from '@/components/QwestCreateDialog'
 import QwestList from '@/components/QwestList'
 
 export default {
   components: {
-    'app-qwest-dialog': QwestDialog,
+    'app-qwest-create-dialog': QwestCreateDialog,
     'app-qwest-list': QwestList
   }
 }
