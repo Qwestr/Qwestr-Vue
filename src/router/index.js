33c3cadc39fd16a15e642bca7b70d38388db9e3e
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-04-21 10:58:16 -0400

    Updated router index to include login/ signup routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,17 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }, {
+    path: '/login',
+    name: 'login'
+    // component: Login
+  }, {
+    path: '/sign-up',
+    name: 'sign-up'
+    // component: SignUp
+  }]
 })
