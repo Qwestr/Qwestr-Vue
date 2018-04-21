6fc58d3fa1b859786931814529a7bc3ba313c114
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-04-21 10:38:06 -0400

    Removed buttons/ data from App component

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,6 @@
       enable-resize-watcher
       v-model="drawer"
       :mini-variant="miniVariant"
-      :clipped="clipped"
-      :fixed="fixed"
     >
       <v-list>
         <v-list-tile
@@ -24,19 +22,10 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
+    <v-toolbar app>
       <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-btn icon @click="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click="fixed = !fixed">
-        <v-icon>remove</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
@@ -44,7 +33,7 @@
     <v-content>
       <router-view/>
     </v-content>
-    <v-footer :fixed="fixed" app>
+    <v-footer app>
       <span>Qwestr &copy; 2018</span>
     </v-footer>
   </v-app>
@@ -54,9 +43,7 @@
 export default {
   data () {
     return {
-      clipped: false,
       drawer: false,
-      fixed: false,
       items: [{
         icon: 'bubble_chart',
         title: 'Qwests'
