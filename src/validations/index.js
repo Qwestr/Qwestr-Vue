0636cd11520d69cc7a1e432966317bcf581fbbc4
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-04-22 22:23:22 -0400

    Created validations folder/ file

---
+++ src/validations/index.js
@@ -0,0 +1,32 @@
+const usernameRequiredRule = v => {
+  return !!v || 'Username is required'
+}
+
+const emailRequiredRule = v => {
+  return !!v || 'Email is required'
+}
+
+const emailValidRule = v => {
+  return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+}
+
+const passwordRequiredRule = v => {
+  return !!v || 'Password is required'
+}
+
+const passwordLengthRule = v => {
+  return (!!v && v.length >= 7) || 'Password must be at least 7 characters long'
+}
+
+const passwordContentRule = v => {
+  return /[0-9]/.test(v) || 'Password must contain at least 1 number'
+}
+
+export default {
+  usernameRequiredRule,
+  emailRequiredRule,
+  emailValidRule,
+  passwordRequiredRule,
+  passwordLengthRule,
+  passwordContentRule
+}
