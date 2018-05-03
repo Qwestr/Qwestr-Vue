613bb404eab2d78e9df19126680013b2954b0f5a
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-03 15:45:23 -0400

    Updated Firebase methods

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,15 +2,12 @@ import Firebase from 'firebase'
 import firebaseConfig from '../../config/firebase/config'
 import store from '@/store'
 
-const firebaseApp = Firebase.initializeApp(firebaseConfig)
-
-const db = firebaseApp.database()
+const db = Firebase.initializeApp(firebaseConfig).database()
 
 const userQwestsRef = () => {
   return db.ref('users/' + store.getters['user'].uid + '/qwests')
 }
 
 export {
-  db,
   userQwestsRef
 }
