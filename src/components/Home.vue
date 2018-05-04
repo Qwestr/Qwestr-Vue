602dfba4bf79cfbdc2b71306a869813fbfe05d34
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-04 15:48:38 -0400

    Removed Qwest component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,35 @@
 <template>
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
-      <app-qwest></app-qwest>
+      <div v-if="isUserLoaded">
+        <app-qwest-create-dialog></app-qwest-create-dialog>
+        <app-qwest-edit-dialog></app-qwest-edit-dialog>
+        <app-qwest-list></app-qwest-list>
+      </div>
+      <div v-else>
+        <app-landing></app-landing>
+      </div>
     </v-slide-y-transition>
   </v-container>
 </template>
 
 <script>
-import Qwest from '@/components/Qwest'
+import Landing from '@/components/Landing'
+import QwestCreateDialog from '@/components/QwestCreateDialog'
+import QwestEditDialog from '@/components/QwestEditDialog'
+import QwestList from '@/components/QwestList'
 
 export default {
   components: {
-    'app-qwest': Qwest
+    'app-landing': Landing,
+    'app-qwest-create-dialog': QwestCreateDialog,
+    'app-qwest-edit-dialog': QwestEditDialog,
+    'app-qwest-list': QwestList
+  },
+  computed: {
+    isUserLoaded () {
+      return this.$store.getters['isUserLoaded']
+    }
   }
 }
 </script>
