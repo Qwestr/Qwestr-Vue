a16e43cf1c3c7a61bb3aa7fee094ceacc9da77d4
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-04 15:50:44 -0400

    Updated QwestDetails component

---
+++ src/components/QwestDetails.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-card>
+        <v-card-title class="headline grey--text">
+          Qwest Details
+        </v-card-title>
+      </v-card>
+    </v-slide-y-transition>
+  </v-container>
+</template>
