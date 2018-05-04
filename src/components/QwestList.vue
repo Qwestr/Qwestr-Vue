ba19c66a01bcdf47cedc99967c8a927fb4bfabc2
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-04 15:10:45 -0400

    Created button to qwest-details page

--- src/components/QwestList.vue
+++ src/components/QwestList.vue
@@ -33,6 +33,9 @@
           </v-btn>
         </td>
         <td class="justify-center layout px-0">
+          <v-btn :to="{ name: 'qwest-details', params: { key: props.item['.key'] } }" icon class="mx-0">
+            <v-icon color="cyan">visibility</v-icon>
+          </v-btn>
           <v-btn @click="editQwest(props.item)" icon class="mx-0">
             <v-icon color="orange">edit</v-icon>
           </v-btn>
