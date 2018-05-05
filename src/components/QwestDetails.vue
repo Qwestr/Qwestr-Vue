9c51377621c10ec7d3d6fa1b0e9a22c1c8f3352c
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-05 13:39:00 -0400

    Updated QwestDetails view

--- src/components/QwestDetails.vue
+++ src/components/QwestDetails.vue
@@ -2,9 +2,19 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-card>
-        <v-card-title class="headline grey--text">
-          Qwest Details
+        <v-card-title primary-title class="headline">
+          {{ qwest.name }}
         </v-card-title>
+        <v-card-text>
+          <div v-if="!qwest.description">
+            <i class="grey--text">
+              No Description Available
+            </i>
+          </div>
+          <div v-else>
+            {{ qwest.description }}
+          </div>
+        </v-card-text>
       </v-card>
     </v-slide-y-transition>
   </v-container>
