56c9495f0b68c2ff0c4fc160f397b1d52ee6b5c3
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-06 20:47:07 -0400

    Created working speed dial for FAB component

--- src/components/FAB.vue
+++ src/components/FAB.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="isFABLoaded">
-    <v-fab-transition>
+    <!-- <v-fab-transition>
       <v-btn
         fab
         dark
@@ -13,12 +13,58 @@
       >
         <v-icon>{{ fab.icon }}</v-icon>
       </v-btn>
-    </v-fab-transition>
+    </v-fab-transition> -->
+    <v-speed-dial
+      fixed
+      bottom
+      right
+      v-model="speedDial"
+    >
+      <v-btn
+        fab
+        dark
+        color="blue"
+        slot="activator"
+        v-model="speedDial"
+      >
+        <v-icon>keyboard_arrow_up</v-icon>
+        <v-icon>keyboard_arrow_down</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="green"
+      >
+        <v-icon>edit</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="indigo"
+      >
+        <v-icon>add</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="red"
+      >
+        <v-icon>delete</v-icon>
+      </v-btn>
+    </v-speed-dial>
 </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      speedDial: false
+    }
+  },
   computed: {
     isFABLoaded () {
       return this.$store.getters['isFABLoaded']
