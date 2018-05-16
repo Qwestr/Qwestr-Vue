632493e7a655ad698cd4182aa3def65403e4f2e5
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-16 12:50:18 -0400

    Created subQwestCreateDialog getter

--- src/store/index.js
+++ src/store/index.js
@@ -36,6 +36,9 @@ export const getters = {
   },
   qwestDetailsEditMode (state) {
     return state.qwestDetailsEditMode
+  },
+  subQwestCreateDialog (state) {
+    return state.subQwestCreateDialog
   }
 }
 
