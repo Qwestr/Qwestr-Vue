a37828fceec5e00adf6fa09a05c86c1c55e4b85f
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-16 22:55:41 -0400

    Added dial buttons for subQwests in FAB component

--- src/components/FAB.vue
+++ src/components/FAB.vue
@@ -61,6 +61,44 @@
         </v-btn>
       </v-speed-dial>
     </v-fab-transition>
+    <v-fab-transition>
+      <v-speed-dial
+        v-show="fab.id === 'SubQwestDetailsFAB'"
+        fixed
+        bottom
+        right
+        v-model="fab.dial"
+      >
+        <v-btn
+          fab
+          dark
+          color="blue"
+          slot="activator"
+          v-model="fab.dial"
+          @click="toggleFABDial"
+        >
+          <v-icon>keyboard_arrow_up</v-icon>
+          <v-icon>close</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          dark
+          small
+          color="red"
+        >
+          <v-icon>delete</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          dark
+          small
+          color="orange"
+          @click="toggleSubQwestDetailsEditMode"
+        >
+          <v-icon>edit</v-icon>
+        </v-btn>
+      </v-speed-dial>
+    </v-fab-transition>
   </div>
 </template>
 
@@ -84,19 +122,25 @@ export default {
       // Dispatch the toggleFABDial action
       this.$store.dispatch('toggleFABDial')
     },
+    toggleQwestCreateDialog () {
+      // Dispatch the toggleQwestCreateDialog action
+      this.$store.dispatch('toggleQwestCreateDialog')
+    },
     toggleQwestDetailsEditMode () {
       // Dispatch the toggleFABDial action
       this.$store.dispatch('toggleFABDial')
       // Dispatch the toggleQwestDetailsEditMode action
       this.$store.dispatch('toggleQwestDetailsEditMode')
     },
-    toggleQwestCreateDialog () {
-      // Dispatch the toggleQwestCreateDialog action
-      this.$store.dispatch('toggleQwestCreateDialog')
-    },
     toggleSubQwestCreateDialog () {
       // Dispatch the toggleSubQwestCreateDialog action
       this.$store.dispatch('toggleSubQwestCreateDialog')
+    },
+    toggleSubQwestDetailsEditMode () {
+      // Dispatch the toggleFABDial action
+      this.$store.dispatch('toggleFABDial')
+      // Dispatch the toggleSubQwestDetailsEditMode action
+      this.$store.dispatch('toggleSubQwestDetailsEditMode')
     }
   }
 }
