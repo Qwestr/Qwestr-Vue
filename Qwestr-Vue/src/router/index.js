8928563b6d27268e08c26d17b5f9a688dce302fe
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-22 13:30:28 -0400

    Copied router/ store/ validations files

--- Qwestr-Vue/src/router/index.js
+++ Qwestr-Vue/src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Login from '@/components/Login'
+import SignUp from '@/components/SignUp'
+import QwestDetails from '@/components/QwestDetails'
+import SubQwestDetails from '@/components/SubQwestDetails'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: Home
+  }, {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }, {
+    path: '/sign-up',
+    name: 'sign-up',
+    component: SignUp
+  }, {
+    path: '/qwest/:key/details',
+    name: 'qwest-details',
+    component: QwestDetails
+  }, {
+    path: '/qwest/:qwestKey/sub-qwest/:subQwestKey/details',
+    name: 'sub-qwest-details',
+    component: SubQwestDetails
+  }]
 })
