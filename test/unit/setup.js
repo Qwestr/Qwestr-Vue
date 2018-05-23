370f87a039cfdfa563878d8e707813d18dc72970
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-23 13:51:34 -0400

    Updated setup file, fixed test for Landing component spec

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,3 +1,7 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+// Setup Vue plugins
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
--- test/unit/specs/components/Landing.spec.js
+++ test/unit/specs/components/Landing.spec.js
@@ -1,13 +1,10 @@
-// import { shallowMount, createLocalVue } from '@vue/test-utils'
-// import Landing from '@/components/FAB'
-
-// Create local Vue instance
-// const localVue = createLocalVue()
+import { shallowMount } from '@vue/test-utils'
+import Landing from '@/components/Landing'
 
 describe('Landing.vue', () => {
   it('should successfully render when created', () => {
     // Mount the component
-    // shallowMount(Landing, { localVue })
+    shallowMount(Landing)
     // Expect that the component has rendered successfully
     expect(true)
   })
