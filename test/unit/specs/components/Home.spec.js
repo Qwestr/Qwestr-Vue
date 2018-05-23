3895a52633d0cd0860488391bba3ccb0c79020fe
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-23 14:09:09 -0400

    Updated tests for Home component spec

--- test/unit/specs/components/Home.spec.js
+++ test/unit/specs/components/Home.spec.js
@@ -1,13 +1,25 @@
-// import { shallowMount, createLocalVue } from '@vue/test-utils'
-// import Home from '@/components/FAB'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Home from '@/components/Home'
 
 // Create local Vue instance
-// const localVue = createLocalVue()
+const localVue = createLocalVue()
+// Load Vuex into local Vue instance
+localVue.use(Vuex)
 
 describe('Home.vue', () => {
+  // Declare mock store values
+  let store, getters
+
+  beforeEach(() => {
+    // Initialize mock getters
+    getters = {}
+    // Initialize mock store
+    store = new Vuex.Store({ getters })
+  })
   it('should successfully render when created', () => {
     // Mount the component
-    // shallowMount(Home, { localVue })
+    shallowMount(Home, { localVue, store })
     // Expect that the component has rendered successfully
     expect(true)
   })
