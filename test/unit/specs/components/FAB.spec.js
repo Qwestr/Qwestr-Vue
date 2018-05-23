b223edecba80ece71b689eadf74fd5ee34fb8eac
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-23 11:42:18 -0400

    Updated tests for FAB component spec

--- test/unit/specs/components/FAB.spec.js
+++ test/unit/specs/components/FAB.spec.js
@@ -1,13 +1,29 @@
 import { shallow, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import FAB from '@/components/FAB'
 
 // Create local Vue instance
 const localVue = createLocalVue()
+// Load Vuex into local Vue instance
+localVue.use(Vuex)
 
 describe('FAB.vue', () => {
+  // Declare mock store values
+  let store, actions
+
+  beforeEach(() => {
+    // Initialize mock actions
+    actions = {
+      'toggleFABDial': jest.fn(),
+      'toggleQwestCreateDialog': jest.fn()
+    }
+    // Initialize mock store
+    store = new Vuex.Store({ actions })
+  })
+
   it('should successfully render when created', () => {
     // Mount the component
-    shallow(FAB, { localVue })
+    shallow(FAB, { localVue, store })
     // Expect that the component has rendered successfully
     expect(true)
   })
