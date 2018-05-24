f77dc135b75e01ee07d97e4906c6c74ebbacfb8e
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-24 16:53:00 -0400

    Updated QwestList component spec

--- test/unit/specs/components/QwestList.spec.js
+++ test/unit/specs/components/QwestList.spec.js
@@ -1,13 +1,26 @@
-// import { shallowMount, createLocalVue } from '@vue/test-utils'
-// import QwestList from '@/components/QwestList'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import QwestList from '@/components/QwestList'
 
 // Create local Vue instance
-// const localVue = createLocalVue()
+const localVue = createLocalVue()
 
 describe('QwestList.vue', () => {
+  // Declare mock store values
+  let store, actions
+
+  beforeEach(() => {
+    // Initialize mock actions
+    actions = {
+      'storeFAB': jest.fn()
+    }
+    // Initialize mock store
+    store = new Vuex.Store({ actions })
+  })
+
   it('should successfully render when created', () => {
     // Mount the component
-    // shallowMount(QwestList, { localVue })
+    shallowMount(QwestList, { localVue, store })
     // Expect that the component has rendered successfully
     expect(true)
   })
