7609b9f098a1096fec9a8eacd27ceb4d383ab0d8
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-24 16:38:53 -0400

    Updated tests for QwestEditDialog component spec

--- test/unit/specs/components/QwestEditDialog.spec.js
+++ test/unit/specs/components/QwestEditDialog.spec.js
@@ -1,13 +1,24 @@
-// import { shallowMount, createLocalVue } from '@vue/test-utils'
-// import QwestEditDialog from '@/components/QwestEditDialog'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import QwestEditDialog from '@/components/QwestEditDialog'
 
 // Create local Vue instance
-// const localVue = createLocalVue()
+const localVue = createLocalVue()
 
 describe('QwestEditDialog.vue', () => {
+  // Declare mock store values
+  let store, getters
+
+  beforeEach(() => {
+    // Initialize mock getters
+    getters = {}
+    // Initialize mock store
+    store = new Vuex.Store({ getters })
+  })
+
   it('should successfully render when created', () => {
     // Mount the component
-    // shallowMount(QwestEditDialog, { localVue })
+    shallowMount(QwestEditDialog, { localVue, store })
     // Expect that the component has rendered successfully
     expect(true)
   })
