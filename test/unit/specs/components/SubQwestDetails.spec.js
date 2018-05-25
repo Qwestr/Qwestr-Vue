37ec0d7af697f0bf81896ef965d1c655e652bbb7
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-25 12:00:33 -0400

    Updated tests for SubQwestDetails component spec

--- test/unit/specs/components/SubQwestDetails.spec.js
+++ test/unit/specs/components/SubQwestDetails.spec.js
@@ -1,13 +1,30 @@
-// import { shallowMount, createLocalVue } from '@vue/test-utils'
-// import SubQwestDetails from '@/components/SubQwestDetails'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import SubQwestDetails from '@/components/SubQwestDetails'
 
 // Create local Vue instance
-// const localVue = createLocalVue()
+const localVue = createLocalVue()
 
 describe('SubQwestDetails.vue', () => {
+  // Declare mock store values
+  let store, getters
+  // Create mock $route
+  const $route = {
+    params: {
+      key: 'sub-qwest-key'
+    }
+  }
+
+  beforeEach(() => {
+    // Initialize mock getters
+    getters = {}
+    // Initialize mock store
+    store = new Vuex.Store({ getters })
+  })
+
   it('should successfully render when created', () => {
     // Mount the component
-    // shallowMount(SubQwestDetails, { localVue })
+    shallowMount(SubQwestDetails, { localVue, store, mocks: { $route } })
     // Expect that the component has rendered successfully
     expect(true)
   })
