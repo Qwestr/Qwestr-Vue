4c2fbc4db47cee9d803469df4c2b9aae1b33ac23
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-25 11:58:05 -0400

    Updated tests for SubQwestCreateDialog component spec

--- test/unit/specs/components/SubQwestCreateDialog.spec.js
+++ test/unit/specs/components/SubQwestCreateDialog.spec.js
@@ -1,13 +1,30 @@
-// import { shallowMount, createLocalVue } from '@vue/test-utils'
-// import SubQwestCreateDialog from '@/components/SubQwestCreateDialog'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import SubQwestCreateDialog from '@/components/SubQwestCreateDialog'
 
 // Create local Vue instance
-// const localVue = createLocalVue()
+const localVue = createLocalVue()
 
 describe('SubQwestCreateDialog.vue', () => {
+  // Declare mock store values
+  let store, getters
+  // Create mock $route
+  const $route = {
+    params: {
+      key: 'sub-qwest-key'
+    }
+  }
+
+  beforeEach(() => {
+    // Initialize mock getters
+    getters = {}
+    // Initialize mock store
+    store = new Vuex.Store({ getters })
+  })
+
   it('should successfully render when created', () => {
     // Mount the component
-    // shallowMount(SubQwestCreateDialog, { localVue })
+    shallowMount(SubQwestCreateDialog, { localVue, store, mocks: { $route } })
     // Expect that the component has rendered successfully
     expect(true)
   })
