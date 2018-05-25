aa6d72583416126723b73b67e9818b1bea2b733e
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-25 12:06:07 -0400

    Updated tests for SubQwestList component spec

--- test/unit/specs/components/SubQwestList.spec.js
+++ test/unit/specs/components/SubQwestList.spec.js
@@ -1,13 +1,20 @@
-// import { shallowMount, createLocalVue } from '@vue/test-utils'
-// import SubQwestList from '@/components/SubQwestList'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import SubQwestList from '@/components/SubQwestList'
 
 // Create local Vue instance
-// const localVue = createLocalVue()
+const localVue = createLocalVue()
 
 describe('SubQwestList.vue', () => {
+  // Create mock $route
+  const $route = {
+    params: {
+      key: 'sub-qwest-key'
+    }
+  }
+
   it('should successfully render when created', () => {
     // Mount the component
-    // shallowMount(SubQwestList, { localVue })
+    shallowMount(SubQwestList, { localVue, mocks: { $route } })
     // Expect that the component has rendered successfully
     expect(true)
   })
